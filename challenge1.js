b432019669572341836a2820c84dc6507529db21
Keltoummalouki <keltoummalouki911@gmail.com>
2024-11-22 23:13:53 +0100

    challenge1

---
+++ challenge1.js
@@ -0,0 +1,16 @@
+function Sum(arr){
+
+    arr = [23,33,3,1,44];
+    if (arr.length >=3 )
+    {
+    let sort = arr.sort((a,b) => b-a);
+    let [a,b,c] = sort;
+    return a + b + c;
+    }
+    else
+    {
+        console.log("null")
+    }
+}
+    arr = [23,33,3,1,44];
+    console.log(Sum(arr));
